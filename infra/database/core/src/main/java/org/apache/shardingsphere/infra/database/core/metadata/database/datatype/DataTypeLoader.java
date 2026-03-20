6477316716d5e99581524e6152f77f6bfe20316f
liufei20151013 <2199216370@qq.com>
2026-03-20 09:34:56 +0800

    feat: loadStandardDataTypes

--- infra/database/core/src/main/java/org/apache/shardingsphere/infra/database/core/metadata/database/datatype/DataTypeLoader.java
+++ infra/database/core/src/main/java/org/apache/shardingsphere/infra/database/core/metadata/database/datatype/DataTypeLoader.java
@@ -47,11 +47,59 @@ public final class DataTypeLoader {
     
     private Map<String, Integer> loadStandardDataTypes(final DatabaseMetaData databaseMetaData) throws SQLException {
         Map<String, Integer> result = new CaseInsensitiveMap<>();
-        try (ResultSet resultSet = databaseMetaData.getTypeInfo()) {
-            while (resultSet.next()) {
-                result.put(resultSet.getString("TYPE_NAME"), resultSet.getInt("DATA_TYPE"));
-            }
-        }
+
+//        try (ResultSet resultSet = databaseMetaData.getTypeInfo()) {
+//            while (resultSet.next()) {
+//                System.out.println("********loadStandardDataTypes TYPE_NAME:" + resultSet.getString("TYPE_NAME"));
+//                System.out.println("********loadStandardDataTypes DATA_TYPE:" + resultSet.getString("DATA_TYPE"));
+//                result.put(resultSet.getString("TYPE_NAME"), resultSet.getInt("DATA_TYPE"));
+//            }
+//        }
+
+        result.put("BIGINT", -5);
+        result.put("BIGINT UNSIGNED", -5);
+        result.put("BINARY", -2);
+        result.put("BIGINT", -5);
+        result.put("BIT", -7);
+        result.put("BLOB", -4);
+        result.put("BOOL", 16);
+        result.put("CHAR", 1);
+        result.put("DATE", 91);
+        result.put("DATETIME", 93);
+        result.put("DECIMAL", 3);
+        result.put("DOUBLE PRECISION", 8);
+        result.put("DOUBLE PRECISION UNSIGNED", 8);
+        result.put("DOUBLE", 8);
+        result.put("DOUBLE UNSIGNED", 8);
+        result.put("ENUM", 1);
+        result.put("FLOAT", 7);
+        result.put("INT", 4);
+        result.put("INT UNSIGNED", 4);
+        result.put("INTEGER", 4);
+        result.put("INTEGER UNSIGNED", 4);
+        result.put("LONG VARBINARY", -4);
+        result.put("LONG VARCHAR", -1);
+        result.put("LONGBLOB", -4);
+        result.put("LONGTEXT", -1);
+        result.put("MEDIUMBLOB", -4);
+        result.put("MEDIUMINT", 4);
+        result.put("MEDIUMINT UNSIGNED", 4);
+        result.put("MEDIUMTEXT", -1);
+        result.put("NUMERIC", 3);
+        result.put("REAL", 8);
+        result.put("SET", 1);
+        result.put("SMALLINT", 5);
+        result.put("SMALLINT UNSIGNED", 5);
+        result.put("TEXT", -1);
+        result.put("TIME", 92);
+        result.put("TIMESTAMP", 93);
+        result.put("TINYBLOB", -3);
+        result.put("TINYINT", -6);
+        result.put("TINYINT UNSIGNED", -6);
+        result.put("TINYTEXT", 12);
+        result.put("VARBINARY", -3);
+        result.put("VARCHAR", 12);
+        result.put("YEAR", 91);
         return result;
     }
 }
